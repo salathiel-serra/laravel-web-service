154d8245c82d50e02c193ef041f6af2e5e1d9430
Salathiel Serra <thiel.serra@gmail.com>
2022-10-04 00:25:38 -0300

    Adicionando arquivos para criação de dados para testes

--- database/migrations/2022_10_03_230321_create_products_table.php
+++ database/migrations/2022_10_03_230321_create_products_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')
+                    ->references('id')
+                    ->on('categories')
+                    ->onDelete('cascade');
+
             $table->string('name', 100)->unique();
             $table->text('description')->nullable();
             $table->string('image')->nullable();
